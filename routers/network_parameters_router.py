34c617151391845e0e06fd63ba7f4db0451b5ec1
anna putilovskaya <putilovskayaam@gmail.com>
2024-12-13 11:13:09 +0300

    add handle_valid_network_ip

--- routers/network_parameters_router.py
+++ routers/network_parameters_router.py
@@ -3,7 +3,8 @@ from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
-from keyboards.common_keyboards import ButtonText
+from filters.network_parameters_filters import valid_network_ip_filter
+from keyboards.common_keyboards import ButtonText, get_prefix_kb
 from routers.states import NetworkParameters
 
 router = Router(name=__name__)
@@ -15,4 +16,17 @@ async def start_get_network_parameters(message: Message, state: FSMContext):
     await state.set_state(NetworkParameters.network_ip)
     await message.answer(
         "Укажите  IP сети в формате 255.255.255.255",
+    )
+
+
+@router.message(NetworkParameters.network_ip, valid_network_ip_filter)
+async def handle_valid_network_ip(
+        message: Message,
+        state: FSMContext,
+):
+    await state.update_data(network=message.text)
+    await state.set_state(NetworkParameters.prefix)
+    await message.answer(
+        "Выберите префикс (битность сети) или отмените ввод:",
+        reply_markup=get_prefix_kb(),
     )
