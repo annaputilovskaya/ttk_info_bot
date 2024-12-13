28b0f7c51e2dd15a9c14c1e2705f2308b64d50dc
anna putilovskaya <putilovskayaam@gmail.com>
2024-12-13 09:43:26 +0300

    create states.py with NetworkParametersStateGroup

--- routers/base_routers.py
+++ routers/base_routers.py
@@ -1,5 +1,6 @@
-from aiogram import Router, types
-from aiogram.filters import CommandStart
+from aiogram import Router
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
 from aiogram.utils import markdown
 
 from keyboards.common_keyboards import get_on_start_kb
@@ -8,11 +9,23 @@ router = Router(name=__name__)
 
 
 @router.message(CommandStart())
-async def cmd_start(message: types.Message):
-
+async def cmd_start(message: Message):
+    """
+    Обрабатывает команду старт.
+    Выводит картинку, приветствие по полному имени пользователя и стартовое меню.
+    """
     url = "https://nanojam.ru/userfiles/img/f0578e3b1a0382df16e64ef718954160.jpg"
 
     await message.answer(
         text=f"{markdown.hide_link(url)}Hello, {markdown.hbold(message.from_user.full_name)}!",
         reply_markup=get_on_start_kb(),
     )
+
+
+@router.message(Command("help"))
+async def cmd_help(message: Message):
+    """
+    Обрабатывает команду помощь.
+    Выводит стартовое меню.
+    """
+    await message.answer(text="Чем могу помочь?", reply_markup=get_on_start_kb())
---
+++ routers/states.py
@@ -0,0 +1,9 @@
+from aiogram.fsm.state import StatesGroup, State
+
+
+class NetworkParameters(StatesGroup):
+    """
+    Состояния получения параметров сети.
+    """
+    network_ip = State()
+    prefix = State()
